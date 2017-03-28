5d3ce3665ae25a0005d72e03b1e5a5b94eb9f5e2
Unknown <dan@walms.co.uk>
2017-03-29 00:40:29 +0100

    control automatically extracts the html source to its running location.

--- Avalonia.SummerNote/SummerNote.cs
+++ Avalonia.SummerNote/SummerNote.cs
@@ -1,9 +1,22 @@
 ﻿using Avalonia.Controls.Primitives;
+using CefGlue.Avalonia;
+using System.IO;
 
 namespace Avalonia.Summernote
 {
     public class Summernote : TemplatedControl
     {
+        protected override void OnTemplateApplied(TemplateAppliedEventArgs e)
+        {
+            var browser = e.NameScope.Find("PART_browser") as AvaloniaCefBrowser;
+            
+            if(browser != null)
+            {
+                var location = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var directory = System.IO.Path.GetDirectoryName(location);                
 
+                browser.StartUrl = $"file:///{Path.Combine(location, directory, "SummerNote").Replace("\\","/")}/editor.html";
+            }
+        }
     }    
 }
